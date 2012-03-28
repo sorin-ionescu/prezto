9155aea2db87326ef377507f34df3e17319bd344
Sorin Ionescu <sorin.ionescu@gmail.com>
2012-03-28 12:23:02 -0400

    Return 1 when GNU Core Utilities are not found

--- plugins/gnu-utils/init.zsh
+++ plugins/gnu-utils/init.zsh
@@ -5,70 +5,73 @@
 #   Sorin Ionescu <sorin.ionescu@gmail.com>
 #
 
-if (( $+commands[gdircolors] )); then
-  function __gnu_utils {
-    emulate -L zsh
-    local gcmds
-    local gcmd
-    local cmd
-    local prefix
+# Check for the presence of GNU Core Utilities.
+if (( ! $+commands[gdircolors] )); then
+  return 1
+fi
 
-    # GNU Coreutils.
-    gcmds=('g[' 'gbase64' 'gbasename' 'gcat' 'gchcon' 'gchgrp' 'gchmod'
-    'gchown' 'gchroot' 'gcksum' 'gcomm' 'gcp' 'gcsplit' 'gcut' 'gdate'
-    'gdd' 'gdf' 'gdir' 'gdircolors' 'gdirname' 'gdu' 'gecho' 'genv' 'gexpand'
-    'gexpr' 'gfactor' 'gfalse' 'gfmt' 'gfold' 'ggroups' 'ghead' 'ghostid'
-    'gid' 'ginstall' 'gjoin' 'gkill' 'glink' 'gln' 'glogname' 'gls' 'gmd5sum'
-    'gmkdir' 'gmkfifo' 'gmknod' 'gmktemp' 'gmv' 'gnice' 'gnl' 'gnohup' 'gnproc'
-    'god' 'gpaste' 'gpathchk' 'gpinky' 'gpr' 'gprintenv' 'gprintf' 'gptx' 'gpwd'
-    'greadlink' 'grm' 'grmdir' 'gruncon' 'gseq' 'gsha1sum' 'gsha224sum'
-    'gsha256sum' 'gsha384sum' 'gsha512sum' 'gshred' 'gshuf' 'gsleep' 'gsort'
-    'gsplit' 'gstat' 'gstty' 'gsum' 'gsync' 'gtac' 'gtail' 'gtee' 'gtest'
-    'gtimeout' 'gtouch' 'gtr' 'gtrue' 'gtruncate' 'gtsort' 'gtty' 'guname'
-    'gunexpand' 'guniq' 'gunlink' 'guptime' 'gusers' 'gvdir' 'gwc' 'gwho'
-    'gwhoami' 'gyes')
+function __gnu_utils {
+  emulate -L zsh
+  local gcmds
+  local gcmd
+  local cmd
+  local prefix
 
-    # Not part of coreutils, installed separately.
-    gcmds+=('ggrep' 'gsed' 'gtar' 'gtime')
+  # GNU Coreutils.
+  gcmds=('g[' 'gbase64' 'gbasename' 'gcat' 'gchcon' 'gchgrp' 'gchmod'
+  'gchown' 'gchroot' 'gcksum' 'gcomm' 'gcp' 'gcsplit' 'gcut' 'gdate'
+  'gdd' 'gdf' 'gdir' 'gdircolors' 'gdirname' 'gdu' 'gecho' 'genv' 'gexpand'
+  'gexpr' 'gfactor' 'gfalse' 'gfmt' 'gfold' 'ggroups' 'ghead' 'ghostid'
+  'gid' 'ginstall' 'gjoin' 'gkill' 'glink' 'gln' 'glogname' 'gls' 'gmd5sum'
+  'gmkdir' 'gmkfifo' 'gmknod' 'gmktemp' 'gmv' 'gnice' 'gnl' 'gnohup' 'gnproc'
+  'god' 'gpaste' 'gpathchk' 'gpinky' 'gpr' 'gprintenv' 'gprintf' 'gptx' 'gpwd'
+  'greadlink' 'grm' 'grmdir' 'gruncon' 'gseq' 'gsha1sum' 'gsha224sum'
+  'gsha256sum' 'gsha384sum' 'gsha512sum' 'gshred' 'gshuf' 'gsleep' 'gsort'
+  'gsplit' 'gstat' 'gstty' 'gsum' 'gsync' 'gtac' 'gtail' 'gtee' 'gtest'
+  'gtimeout' 'gtouch' 'gtr' 'gtrue' 'gtruncate' 'gtsort' 'gtty' 'guname'
+  'gunexpand' 'guniq' 'gunlink' 'guptime' 'gusers' 'gvdir' 'gwc' 'gwho'
+  'gwhoami' 'gyes')
 
-    for gcmd in "$gcmds[@]"; do
-      #
-      # This method allows for builtin commands to be primary but it's
-      # lost if hash -r or rehash -f is executed. Thus, those two
-      # functions have to be wrapped.
-      #
-      if (( $+commands[$gcmd] )); then
-        hash "$gcmd[2,-1]"="$commands[$gcmd]"
-      fi
-    done
+  # Not part of coreutils, installed separately.
+  gcmds+=('ggrep' 'gsed' 'gtar' 'gtime')
 
-    return 0
-  }
-  __gnu_utils;
-
-  function hash {
-    if (( $+argv[(er)-r] )) || (( $+argv[(er)-f] )); then
-      builtin hash "$@"
-      __gnu_utils
-    else
-      builtin hash "$@"
+  for gcmd in "$gcmds[@]"; do
+    #
+    # This method allows for builtin commands to be primary but it's
+    # lost if hash -r or rehash -f is executed. Thus, those two
+    # functions have to be wrapped.
+    #
+    if (( $+commands[$gcmd] )); then
+      hash "$gcmd[2,-1]"="$commands[$gcmd]"
     fi
-  }
+  done
 
-  function rehash {
-    hash -r "$@"
-  }
+  return 0
+}
+__gnu_utils;
 
-  # A sensible default for ls.
-  alias ls='ls --group-directories-first'
-
-  if zstyle -t ':omz:alias:ls' color; then
-    if [[ -f "$HOME/.dir_colors" ]]; then
-      eval $(gdircolors "$HOME/.dir_colors")
-    fi
-    alias ls="$aliases[ls] --color=auto"
+function hash {
+  if (( $+argv[(er)-r] )) || (( $+argv[(er)-f] )); then
+    builtin hash "$@"
+    __gnu_utils
   else
-    alias ls="$aliases[ls] -F"
+    builtin hash "$@"
   fi
+}
+
+function rehash {
+  hash -r "$@"
+}
+
+# A sensible default for ls.
+alias ls='ls --group-directories-first'
+
+if zstyle -t ':omz:alias:ls' color; then
+  if [[ -f "$HOME/.dir_colors" ]]; then
+    eval $(gdircolors "$HOME/.dir_colors")
+  fi
+  alias ls="$aliases[ls] --color=auto"
+else
+  alias ls="$aliases[ls] -F"
 fi
 
