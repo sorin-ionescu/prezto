7f7d1336788f77f113b37679df1b48dc620f7f1c
Sorin Ionescu <sorin.ionescu@gmail.com>
2012-03-29 10:47:30 -0400

    [Fix #4] Add zsh-syntax-highlighting

---
+++ plugins/syntax-highlighting/init.zsh
@@ -0,0 +1,12 @@
+#
+# Integrates zsh-syntax-highlighting into Oh My Zsh.
+#
+# Authors:
+#   Sorin Ionescu <sorin.ionescu@gmail.com>
+#
+
+if zstyle -t ':omz:plugin:syntax-highlighting' color; then
+  source "${0:h}/external/zsh-syntax-highlighting.zsh"
+  ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
+fi
+
