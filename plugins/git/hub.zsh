61c1c293be28a3f85640e65c48e01b9538aac29a
Colin Hebert <hebert.colin@gmail.com>
2012-03-29 23:22:24 +0100

    Use the "hub alias -s" command to setup hub

--- plugins/git/hub.zsh
+++ plugins/git/hub.zsh
@@ -1,14 +1,14 @@
 #
 # Adds GitHub knowledge to the Git command.
+# https://github.com/defunkt/hub
 #
 # Authors:
 #   Chris Wanstrath <chris@wanstrath.com>
 #   Sorin Ionescu <sorin.ionescu@gmail.com>
+#   Colin Hebert <hebert.colin@gmail.com>
 #
 
 if (( $+commands[hub] )); then
-  function git {
-    hub "$@"
-  }
+  eval "$(hub alias -s)"
 fi
 
