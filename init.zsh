9fc437439bbdc86ac1a20296d9d2144d07ae1708
Sorin Ionescu <sorin.ionescu@gmail.com>
2012-03-31 13:51:40 -0400

    Rename keyboard to editor
    It is not the keyboard that is being set up but the Zsh Line Editor
    (ZLE).

--- init.zsh
+++ init.zsh
@@ -37,7 +37,7 @@ autoload -Uz compinit && compinit -i
 source "${0:h}/helper.zsh"
 source "${0:h}/environment.zsh"
 source "${0:h}/terminal.zsh"
-source "${0:h}/keyboard.zsh"
+source "${0:h}/editor.zsh"
 source "${0:h}/completion.zsh"
 source "${0:h}/history.zsh"
 source "${0:h}/directory.zsh"
