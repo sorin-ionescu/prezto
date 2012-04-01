6d38d9836b1e71cab05f6f685d2a308dfb2061ae
Sorin Ionescu <sorin.ionescu@gmail.com>
2012-03-31 20:49:54 -0400

    Fix node cache file sourcing

--- plugins/node/init.zsh
+++ plugins/node/init.zsh
@@ -6,10 +6,12 @@
 #
 
 cache_file="${0:h}/cache.zsh"
-if [[ ! -f "$cache_file" ]] && (( $+commands[npm] )); then
-  # npm is slow; cache its output.
-  npm completion >! "$cache_file" 2> /dev/null
-  source "$cache_file"
+if [[ ! -f "$cache_file" ]]; then
+  if (( $+commands[npm] )); then
+    # npm is slow; cache its output.
+    npm completion >! "$cache_file" 2> /dev/null
+    source "$cache_file"
+  fi
 else
   source "$cache_file"
 fi
