b47b845b13600bbee9fe5e0139fe5b98b346b266
Sorin Ionescu <sorin.ionescu@gmail.com>
2012-04-04 14:03:38 -0400

    Move history-stat from utility into history

--- modules/history/init.zsh
+++ modules/history/init.zsh
@@ -24,3 +24,8 @@ setopt HIST_REDUCE_BLANKS        # Remove superfluous blanks before recording en
 setopt HIST_VERIFY               # Don't execute immediately upon history expansion.
 setopt HIST_BEEP                 # Beep when accessing nonexistent history.
 
+# Aliases
+
+# Lists the ten most used commands.
+alias history-stat="history . | awk '{print \$2}' | sort | uniq -c | sort -n -r | head"
+
