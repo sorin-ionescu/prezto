0165379143a19c1f4ed4d2d0328b3ea41a8265a2
Sorin Ionescu <sorin.ionescu@gmail.com>
2012-05-20 16:12:56 -0400

    Clarify comments in ruby module

--- modules/ruby/init.zsh
+++ modules/ruby/init.zsh
@@ -5,22 +5,26 @@
 #   Sorin Ionescu <sorin.ionescu@gmail.com>
 #
 
-# Loads RVM into the shell session.
+# Load RVM into the shell session.
 if [[ -s "$HOME/.rvm/scripts/rvm" ]]; then
-  # Auto adding variable-stored paths to ~ list conflicts with RVM.
+  # Unset AUTO_NAME_DIRS since auto adding variable-stored paths to ~ list
+  # conflicts with RVM.
   unsetopt AUTO_NAME_DIRS
 
   # Source RVM.
   source "$HOME/.rvm/scripts/rvm"
-# Loads manually installed rbenv into the shell session.
+
+# Load manually installed rbenv into the shell session.
 elif [[ -s "$HOME/.rbenv/bin/rbenv" ]]; then
   path=("$HOME/.rbenv/bin" $path)
   eval "$(rbenv init - zsh)"
-# Loads package manager installed rbenv into the shell session.
+
+# Load package manager installed rbenv into the shell session.
 elif (( $+commands[rbenv] )); then
   eval "$(rbenv init - zsh)"
+
+# Install local gems according to operating system conventions.
 else
-  # Install local gems according to Mac OS X conventions.
   if [[ "$OSTYPE" == darwin* ]]; then
     export GEM_HOME="$HOME/Library/Ruby/Gems/1.8"
     path=("$GEM_HOME/bin" $path)
