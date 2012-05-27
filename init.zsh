39a60008e5e0bcd0d3a9d96a2f73b13db70528e3
Sorin Ionescu <sorin.ionescu@gmail.com>
2012-05-27 19:43:06 -0400

    [#171] Move Zsh file compilation into zlogin

--- init.zsh
+++ init.zsh
@@ -37,10 +37,3 @@ zstyle -a ':omz:load' omodule 'omodules'
 omodload "$omodules[@]"
 unset omodules
 
-# Compile the completion dump to increase startup speed.
-dump_file="$HOME/.zcompdump"
-if [[ "$dump_file" -nt "${dump_file}.zwc" || ! -s "${dump_file}.zwc" ]]; then
-  zcompile "$dump_file"
-fi
-unset dump_file
-
