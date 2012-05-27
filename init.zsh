1645fb88bd7fb5786dbad8e7d5814ac6b0be551f
Sorin Ionescu <sorin.ionescu@gmail.com>
2012-05-27 17:04:01 -0400

    [#171] Move launchd setenv into zlogin

--- init.zsh
+++ init.zsh
@@ -37,15 +37,7 @@ zstyle -a ':omz:load' omodule 'omodules'
 omodload "$omodules[@]"
 unset omodules
 
-# Set environment variables for launchd processes.
-if [[ "$OSTYPE" == darwin* ]]; then
-  for env_var in PATH MANPATH; do
-    launchctl setenv "$env_var" "${(P)env_var}" &!
-  done
-  unset env_var
-fi
-
-# Compile the completion dump, to increase startup speed.
+# Compile the completion dump to increase startup speed.
 dump_file="$HOME/.zcompdump"
 if [[ "$dump_file" -nt "${dump_file}.zwc" || ! -s "${dump_file}.zwc" ]]; then
   zcompile "$dump_file"
