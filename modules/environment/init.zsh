4fd347580ec4a4d15b8baef79efacd69fa7df042
Sorin Ionescu <sorin.ionescu@gmail.com>
2012-05-31 17:50:06 -0400

    [Fix #104] Add documentation for environment

--- modules/environment/init.zsh
+++ modules/environment/init.zsh
@@ -30,12 +30,12 @@ fi
 
 # Termcap
 if zstyle -t ':omz:environment:termcap' color; then
-  export LESS_TERMCAP_mb=$'\E[01;31m'      # begin blinking
-  export LESS_TERMCAP_md=$'\E[01;31m'      # begin bold
-  export LESS_TERMCAP_me=$'\E[0m'          # end mode
-  export LESS_TERMCAP_se=$'\E[0m'          # end standout-mode
-  export LESS_TERMCAP_so=$'\E[00;47;30m'   # begin standout-mode
-  export LESS_TERMCAP_ue=$'\E[0m'          # end underline
-  export LESS_TERMCAP_us=$'\E[01;32m'      # begin underline
+  export LESS_TERMCAP_mb=$'\E[01;31m'      # Begins blinking.
+  export LESS_TERMCAP_md=$'\E[01;31m'      # Begins bold.
+  export LESS_TERMCAP_me=$'\E[0m'          # Ends mode.
+  export LESS_TERMCAP_se=$'\E[0m'          # Ends standout-mode.
+  export LESS_TERMCAP_so=$'\E[00;47;30m'   # Begins standout-mode.
+  export LESS_TERMCAP_ue=$'\E[0m'          # Ends underline.
+  export LESS_TERMCAP_us=$'\E[01;32m'      # Begins underline.
 fi
 
