c352b86aeb76206c999e60da7823767bef2cf9d2
Sorin Ionescu <sorin.ionescu@gmail.com>
2012-06-02 14:19:33 -0400

    Add MacPorts paths to path variables

--- modules/macports/init.zsh
+++ modules/macports/init.zsh
@@ -10,6 +10,26 @@ if [[ "$OSTYPE" != darwin* ]]; then
   return 1
 fi
 
+# Paths
+
+# Set the list of directories that info searches for manuals.
+infopath=(
+  /opt/local/share/info
+  $infopath
+)
+
+# Set the list of directories that man searches for manuals.
+manpath=(
+  /opt/local/share/man
+  $manpath
+)
+
+# Set the list of directories that Zsh searches for programs.
+path=(
+  /opt/local/{bin,sbin}
+  $path
+)
+
 # Aliases
 alias pc="sudo port clean --all installed"
 alias pi="sudo port install $1"
