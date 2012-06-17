2a0a7b30616ef1fe820652391055df6dd4144284
Ben O'Hara <bohara@gmail.com>
2012-06-17 14:53:14 +1000

    dont load the prompt module if older zsh version

--- modules/prompt/init.zsh
+++ modules/prompt/init.zsh
@@ -8,6 +8,13 @@
 # Load and execute the prompt theming system.
 autoload -Uz promptinit && promptinit
 
+# Check for the minimum supported version.
+min_zsh_version='4.3.10'
+if ! autoload -Uz is-at-least || ! is-at-least "$min_zsh_version"; then
+  return 1
+fi
+unset min_zsh_version
+
 # Load the prompt theme.
 zstyle -a ':omz:module:prompt' theme 'prompt_argv'
 if (( $#prompt_argv > 0 )); then
