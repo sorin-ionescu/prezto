2250f93fa3d9deaddbf4b8c09401496a087d8436
Sorin Ionescu <sorin.ionescu@gmail.com>
2012-08-07 20:38:14 -0400

    [Fix #233] Bind history-substring-search using $key_info

--- modules/history-substring-search/init.zsh
+++ modules/history-substring-search/init.zsh
@@ -24,8 +24,17 @@ fi
 # Key Bindings
 #
 
+# Emacs
 bindkey -M emacs "$key_info[Control]P" history-substring-search-up
 bindkey -M emacs "$key_info[Control]N" history-substring-search-down
+
+# Vi
 bindkey -M vicmd "k" history-substring-search-up
 bindkey -M vicmd "j" history-substring-search-down
+
+# Emacs and Vi
+for keymap in 'emacs' 'viins'; do
+  bindkey -M "$keymap" "$key_info[Up]" history-substring-search-up
+  bindkey -M "$keymap" "$key_info[Down]" history-substring-search-down
+done
 
