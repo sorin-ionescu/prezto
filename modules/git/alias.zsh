935e6a5060a83409f9826033c7d55e07bfce9cce
Sorin Ionescu <sorin@krustyroo.local>
2012-08-31 21:07:50 -0400

    Show references in Git one line and graph formats

--- modules/git/alias.zsh
+++ modules/git/alias.zsh
@@ -72,7 +72,7 @@ alias gkT='gkt $(gkl)'
 
 # Log (l)
 git_log_format_medium='--pretty=format:%C(bold)Commit:%C(reset) %C(green)%H%C(red)%d%n%C(bold)Author:%C(reset) %C(cyan)%an <%ae>%n%C(bold)Date:%C(reset)   %C(blue)%ai (%ar)%C(reset)%n%+B'
-git_log_format_oneline='--pretty=format:%C(green)%h%C(reset) %s%n'
+git_log_format_oneline='--pretty=format:%C(green)%h%C(reset) %s%C(red)%d%C(reset)%n'
 git_log_format_brief='--pretty=format:%C(green)%h%C(reset) %s%n%C(blue)(%ar by %an)%C(red)%d%C(reset)%n'
 
 alias gl='git log --topo-order ${git_log_format_medium}'
