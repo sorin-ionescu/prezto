10f333f45c344a5c13b516b963e22bb3c586108e
Sorin Ionescu <sorin@krustyroo.local>
2012-09-08 19:08:54 -0400

    Rename autoloadable to is-autoloadable

--- modules/helper/init.zsh
+++ modules/helper/init.zsh
@@ -5,15 +5,20 @@
 #   Sorin Ionescu <sorin.ionescu@gmail.com>
 #
 
-# Checks a boolean variable for "true".
-# Case insensitive: "1", "y", "yes", "t", "true", "o", and "on".
-function is-true {
-  [[ -n "$1" && "$1" == (1|[Yy]([Ee][Ss]|)|[Tt]([Rr][Uu][Ee]|)|[Oo]([Nn]|)) ]]
+# Checks if a file can be autoloaded by trying to load it in a subshell.
+function is-autoloadable {
+  ( unfunction $1 ; autoload -U +X $1 ) &> /dev/null
 }
 
 # Checks a name if it is a command, function, or alias.
 function is-callable {
   (( $+commands[$1] )) || (( $+functions[$1] )) || (( $+aliases[$1] ))
+}
+
+# Checks a boolean variable for "true".
+# Case insensitive: "1", "y", "yes", "t", "true", "o", and "on".
+function is-true {
+  [[ -n "$1" && "$1" == (1|[Yy]([Ee][Ss]|)|[Tt]([Rr][Uu][Ee]|)|[Oo]([Nn]|)) ]]
 }
 
 # Prints the first non-empty string in the arguments array.
@@ -23,10 +28,5 @@ function coalesce {
     return 0
   done
   return 1
-}
-
-# Checks if a file can be autoloaded by trying to load it in a subshell.
-function autoloadable {
-  ( unfunction $1 ; autoload -U +X $1 ) &> /dev/null
 }
 
