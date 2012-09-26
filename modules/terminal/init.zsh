be0e4245a6b693a0e0fd5cfa2cad8add503442ef
Sorin Ionescu <sorin@krustyroo.local>
2012-09-26 16:51:32 -0400

    [Fix #301] Unset $MATCH after use

--- modules/terminal/init.zsh
+++ modules/terminal/init.zsh
@@ -68,6 +68,7 @@ function set-titles-with-command {
     # Set the command name, or in the case of sudo or ssh, the next command.
     local cmd=${${1[(wr)^(*=*|sudo|ssh|-*)]}:t}
     local truncated_cmd="${cmd/(#m)?(#c15,)/${MATCH[1,12]}...}"
+    unset MATCH
 
     if [[ "$TERM" == screen* ]]; then
       set-screen-window-title "$truncated_cmd"
@@ -90,6 +91,7 @@ function set-titles-with-path {
   else
     local abbreviated_path="${absolute_path/#$HOME/~}"
     local truncated_path="${abbreviated_path/(#m)?(#c15,)/...${MATCH[-12,-1]}}"
+    unset MATCH
 
     if [[ "$TERM" == screen* ]]; then
       set-screen-window-title "$truncated_path"
