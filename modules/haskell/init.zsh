7385dc35ac50466e493cbebfadd1e7c1284da057
Sebastian Wiesner <lunaryorn@gmail.com>
2012-10-11 14:23:41 +0200

    Fallback to standard cabal location on OS X
    If ~/Library/Haskell does not exist, fall back to ~/.cabal/bin.
    
    Ideally, we'd parse ~/.cabal/config here, but cabal does not provide an
    interface to get configuration settings.

--- modules/haskell/init.zsh
+++ modules/haskell/init.zsh
@@ -11,11 +11,10 @@ if (( ! $+commands[ghc] )); then
 fi
 
 # Prepend Cabal per user directories to PATH/MANPATH.
-if [[ "$OSTYPE" == darwin* ]]; then
+if [[ "$OSTYPE" == darwin* && -d $HOME/Library/Haskell ]]; then
   path=($HOME/Library/Haskell/bin(/N) $path)
   manpath=($HOME/Library/Haskell/man(/N) $manpath)
 else
   path=($HOME/.cabal/bin(/N) $path)
   manpath=($HOME/.cabal/man(/N) $manpath)
 fi
-
