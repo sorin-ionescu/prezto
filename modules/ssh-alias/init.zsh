1971c3aaf0b3e06f7c2580afa1041250041d8a71
Roey Darwish Dror <roey.ghost@gmail.com>
2012-11-02 11:25:58 +0200

    Added documentation for ssh-alias

--- modules/ssh-alias/init.zsh
+++ modules/ssh-alias/init.zsh
@@ -1,3 +1,10 @@
+#
+# Create aliases for hosts defined in ~/.ssh/config
+#
+# Authors:
+#   Roey Darwish Dror <roey.ghost@gmail.com>
+#
+
 local x
 for x in $(grep 'Host ' ~/.ssh/config | awk '{ print $2 }'); do
     alias $x="ssh $x"
