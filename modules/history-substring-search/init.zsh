80b203bcd32a8e5ac22dac36d7e48e3f9ccf7078
Sorin Ionescu <sorin.ionescu@gmail.com>
2013-04-25 17:51:27 -0400

    [Fix #422] Revert "Remove no longer necessary key bindings"
    This reverts commit 3c5363ef9bcfda4cc5cd46ca85a7109f783c9539.

--- modules/history-substring-search/init.zsh
+++ modules/history-substring-search/init.zsh
@@ -24,3 +24,23 @@ if ! zstyle -t ':prezto:module:history-substring-search' color; then
   unset HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_{FOUND,NOT_FOUND}
 fi
 
+#
+# Key Bindings
+#
+
+if [[ -n $key_info ]]; then
+  # Emacs
+  bindkey -M emacs "$key_info[Control]P" history-substring-search-up
+  bindkey -M emacs "$key_info[Control]N" history-substring-search-down
+
+  # Vi
+  bindkey -M vicmd "k" history-substring-search-up
+  bindkey -M vicmd "j" history-substring-search-down
+
+  # Emacs and Vi
+  for keymap in 'emacs' 'viins'; do
+    bindkey -M "$keymap" "$key_info[Up]" history-substring-search-up
+    bindkey -M "$keymap" "$key_info[Down]" history-substring-search-down
+  done
+fi
+
