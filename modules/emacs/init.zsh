fb5b1be345bf463f90aaa0b14f92ec9c5868523f
Sorin Ionescu <sorin.ionescu@gmail.com>
2013-05-17 21:29:44 -0400

    Refactor Emacs module

--- modules/emacs/init.zsh
+++ modules/emacs/init.zsh
@@ -4,17 +4,22 @@
 # Authors: Sebastian Wiesner <lunaryorn@gmail.com>
 #
 
-# Enable Carton
-if [[ -d "$HOME/.carton" ]]; then
-    path=($HOME/.carton/bin $path)
-
-    alias cai='carton install'
-    alias cau='carton update'
-    alias caI='carton init'
-    alias cae='carton exec'
-
-    local completion="$HOME/.carton/etc/carton_completion.zsh"
-    if [[ -e "${completion}" ]]; then
-      source "${completion}"
-    fi
+# Return if requirements are not found.
+if [[ ! -d "$HOME/.carton" ]]; then
+  return 1
 fi
+
+# Prepend Carton bin directory.
+path=($HOME/.carton/bin $path)
+
+# Load Carton completion
+source "$HOME/.carton/etc/carton_completion.zsh" 2> /dev/null
+
+#
+# Aliases
+#
+
+alias cai='carton install'
+alias cau='carton update'
+alias caI='carton init'
+alias cae='carton exec'
