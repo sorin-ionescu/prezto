91bc945510392c957d15c4c88509e45f6c1decde
Chauncey Garrett <vrtcl1dvoshun+github@gmail.com>
2014-03-25 18:17:56 -0500

    Added aliases for `ls`-ing hidden files only.
    [ticket: X]

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -91,17 +91,19 @@ else
   fi
 fi
 
-alias l='ls -1A'         # Lists in one column, hidden files.
-alias ll='ls -lh'        # Lists human readable sizes.
-alias lr='ll -R'         # Lists human readable sizes, recursively.
-alias la='ll -A'         # Lists human readable sizes, hidden files.
-alias lm='la | "$PAGER"' # Lists human readable sizes, hidden files through pager.
-alias lx='ll -XB'        # Lists sorted by extension (GNU only).
-alias lk='ll -Sr'        # Lists sorted by size, largest last.
-alias lt='ll -tr'        # Lists sorted by date, most recent last.
-alias lc='lt -c'         # Lists sorted by date, most recent last, shows change time.
-alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
-alias sl='ls'            # I often screw this up.
+alias lsd='ls -A | egrep "^\."' # Lists hidden files only.
+alias l='ls -1A'                # Lists in one column, hidden files.
+alias ll='ls -lh'               # Lists human readable sizes.
+alias lr='ll -R'                # Lists human readable sizes, recursively.
+alias la='ll -A'                # Lists human readable sizes, hidden files.
+alias lld='la | egrep "^\."'    # Lists human readable sizes, hidden files only.
+alias lm='la | "$PAGER"'        # Lists human readable sizes, hidden files through pager.
+alias lx='ll -XB'               # Lists sorted by extension (GNU only).
+alias lk='ll -Sr'               # Lists sorted by size, largest last.
+alias lt='ll -tr'               # Lists sorted by date, most recent last.
+alias lc='lt -c'                # Lists sorted by date, most recent last, shows change time.
+alias lu='lt -u'                # Lists sorted by date, most recent last, shows access time.
+alias sl='ls'                   # I often screw this up.
 
 # Mac OS X Everywhere
 if [[ "$OSTYPE" == darwin* ]]; then
