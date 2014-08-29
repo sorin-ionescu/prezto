4f7710c781767e8c024048d6558b501652069e98
Chauncey Garrett <vrtcl1dvoshun+github@gmail.com>
2014-08-29 11:31:46 -0500

    Set CLICOLOR to colorize ls
    `ls --color-auto` is not correct of OS X. Set the environment variable
    instead (same as using ls -G).
    
    [ticket: X]

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -86,7 +86,8 @@ else
     # Define colors for the completion system.
     export LS_COLORS='di=34:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'
 
-    alias ls='ls --color=auto'
+	# Enable colors for the command line interface.
+	export CLICOLOR=1
   else
     alias ls='ls -F'
   fi
