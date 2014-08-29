0d66550736156f37a1e1ce82277b9ff906e0ee88
Chauncey Garrett <vrtcl1dvoshun+github@gmail.com>
2014-08-29 11:30:57 -0500

    Use VISUAL instead of EDITOR for v
    [ticket: X]

--- modules/fasd/aliases.zsh
+++ modules/fasd/aliases.zsh
@@ -3,6 +3,6 @@
 #
 
 alias j='fasd_cd -id'                    # Changes the current working directory interactively.
-alias v='fasd -f -t -e $EDITOR'             # use Vim's builtin list of accessed files (similar rupa/v)
+alias v='fasd -f -t -e $VISUAL'             # use Vim's builtin list of accessed files (similar rupa/v)
 # alias v='fasd -f -e vim'                 #
 # alias v='fasd -f -t -e vim -b viminfo'   # use Vim's builtin list of accessed files (similar rupa/v)
