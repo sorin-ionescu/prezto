ef6153791e6c6524a46632f8de65d85573502f51
Humberto Morales <hmorales@gmail.com>
2014-09-02 16:29:18 -0700

    Removed lazy version of virtualenvwrapper since it was causing a crash in workon.

--- modules/python/init.zsh
+++ modules/python/init.zsh
@@ -35,14 +35,14 @@ if (( ! $+commands[python] && ! $+commands[pyenv] )); then
 fi
 
 # Load virtualenvwrapper into the shell session.
-if (( $+commands[virtualenvwrapper_lazy.sh] )); then
+if (( $+commands[virtualenvwrapper.sh] )); then
   # Set the directory where virtual environments are stored.
   export WORKON_HOME="$HOME/.virtualenvs"
 
   # Disable the virtualenv prompt.
   VIRTUAL_ENV_DISABLE_PROMPT=1
 
-  source "$commands[virtualenvwrapper_lazy.sh]"
+  source "$commands[virtualenvwrapper.sh]"
 fi
 
 #
