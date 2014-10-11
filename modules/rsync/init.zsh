fec7daa5067feaf6aeabd8c0670d3d9e45e5215d
Indrajit Raychaudhuri <irc@indrajit.com>
2014-10-11 23:27:04 +0530

    Enable 'acls', 'xattrs' support in rsync whenever available.
    rsync has support for these available in most cases and isn't
    specific to Mac OS X any more.

--- modules/rsync/init.zsh
+++ modules/rsync/init.zsh
@@ -16,10 +16,14 @@ fi
 
 _rsync_cmd='rsync --verbose --progress --human-readable --compress --archive --hard-links --one-file-system'
 
+if grep -q 'xattrs' <(rsync --help 2>&1); then
+  _rsync_cmd="${_rsync_cmd} --acls --xattrs"
+fi
+
 # Mac OS X and HFS+ Enhancements
 # http://help.bombich.com/kb/overview/credits#opensource
 if [[ "$OSTYPE" == darwin* ]] && grep -q 'file-flags' <(rsync --help 2>&1); then
-  _rsync_cmd="${_rsync_cmd} --crtimes --acls --xattrs --fileflags --protect-decmpfs --force-change"
+  _rsync_cmd="${_rsync_cmd} --crtimes --fileflags --protect-decmpfs --force-change"
 fi
 
 alias rsync-copy="${_rsync_cmd}"
