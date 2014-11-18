d5127fae6c5e6f02eea5de28700d5f8900d51f1d
mkcode <chrisewald@gmail.com>
2014-11-19 00:16:14 +1100

    fix whitespace to 2 spaces

--- modules/pj/init.zsh
+++ modules/pj/init.zsh
@@ -14,38 +14,35 @@
 #
 
 function pj() {
-        cmd="cd"
-    file=$1
+  cmd="cd"
+  file=$1
 
-    if [[ "open" == "$file" ]] then
-        shift
-        file=$*
-        cmd=(${(s: :)EDITOR})
-    else
-        file=$*
+  if [[ "open" == "$file" ]] then
+    shift
+    file=$*
+    cmd=(${(s: :)EDITOR})
+  else
+    file=$*
+  fi
+
+  for project in $PROJECT_PATHS; do
+    if [[ -d $project/$file ]] then
+      $cmd "$project/$file"
+      unset project # Unset project var
+      return
     fi
-
-    for project in $PROJECT_PATHS; do
-        if [[ -d $project/$file ]] then
-            $cmd "$project/$file"
-            unset project # Unset project var
-            return
-        fi
-    done
-
-    echo "No such project $1"
-
+  done
+  echo "No such project $1"
 }
 
 alias pjo="pj open"
 
 function _pj () {
-        # might be possible to improve this using glob, without the basename trick
-    typeset -a projects
-    projects=($PROJECT_PATHS/*)
-    projects=$projects:t
-    _arguments '*:file:($projects)'
-
+  # might be possible to improve this using glob, without the basename trick
+  typeset -a projects
+  projects=($PROJECT_PATHS/*)
+  projects=$projects:t
+  _arguments '*:file:($projects)'
 }
 
 compdef _pj pj
