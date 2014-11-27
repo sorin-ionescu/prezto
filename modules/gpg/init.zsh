fc3f8025ea806a20aa0f7a2af9ef6f2be8b3b1a6
Grégoire Détrez <gregoire.detrez@gu.se>
2014-11-27 04:19:26 +0100

    Add a preexec hook to set the GPG-Agent TTY
    Signed-off-by: Sorin Ionescu <sorin.ionescu@gmail.com>

--- modules/gpg/init.zsh
+++ modules/gpg/init.zsh
@@ -35,6 +35,12 @@ if grep 'enable-ssh-support' "$_gpg_agent_conf" &> /dev/null; then
 
   # Load the SSH module for additional processing.
   pmodload 'ssh'
+
+  # Updates the GPG-Agent TTY before every command since SSH does not set it.
+  function _gpg-agent-update-tty {
+    gpg-connect-agent UPDATESTARTUPTTY /bye >/dev/null
+  }
+  add-zsh-hook preexec _gpg-agent-update-tty
 fi
 
 # Clean up.
