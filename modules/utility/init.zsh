sorin-ionescu/prezto

commit 933c61b8f1af64e745260f3c92d32fa08399688d

[Fix #776] Support GNU top

modules/utility/init.zsh
@@ -146,8 +146,13 @@ alias du='du -kh'
 if (( $+commands[htop] )); then
   alias top=htop
 else
-  alias topc='top -o cpu'
-  alias topm='top -o vsize'
+  if [[ "$OSTYPE" == darwin*|*bsd* ]]; then
+    alias topc='top -o cpu'
+    alias topm='top -o vsize'
+  else
+    alias topc='top -o %CPU'
+    alias topm='top -o %MEM'
+  fi
 fi
 
 # Miscellaneous

commit 0344b6439b920be8678e2ae7bd3daf2104439666

Add missing parentheses
Fixes parse error on init introduced by 933c61b8f1af64e745260f3c92d32fa08399688d

modules/utility/init.zsh
@@ -146,7 +146,7 @@ alias du='du -kh'
 if (( $+commands[htop] )); then
   alias top=htop
 else
-  if [[ "$OSTYPE" == darwin*|*bsd* ]]; then
+  if [[ "$OSTYPE" == (darwin*|*bsd*) ]]; then
     alias topc='top -o cpu'
     alias topm='top -o vsize'
   else