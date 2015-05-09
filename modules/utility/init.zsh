d8b7ff6a141056770bbddda72b71921775272b1c
plessbd <plessbd@users.noreply.github.com>
2015-05-09 03:25:07 -0400

    Update Utility http-serve
    start a SimpleHTTPServer with UTF-8 encoding
    http://stackoverflow.com/questions/15288891/how-can-i-serve-files-with-utf-8-encoding-using-python-simplehttpserver#24517632

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -158,7 +158,7 @@ fi
 # Miscellaneous
 
 # Serves a directory via HTTP.
-alias http-serve='python -m SimpleHTTPServer'
+alias http-serve="python -c \"import SimpleHTTPServer; m = SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map; m[''] = 'text/plain'; m.update(dict([(k, v + ';charset=UTF-8') for k, v in m.items()])); SimpleHTTPServer.test();\""
 
 #
 # Functions
