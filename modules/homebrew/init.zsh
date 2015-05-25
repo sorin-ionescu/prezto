b4dc724b8429cccf53d64c0c9c44d2de2211b3f7
Pavel Skvazh <pavel.skvazh@gmail.com>
2015-05-25 11:28:42 +0300

    Update init.zsh
    This will be required in the upcoming version of brew

--- modules/homebrew/init.zsh
+++ modules/homebrew/init.zsh
@@ -21,7 +21,7 @@ alias brewi='brew install'
 alias brewl='brew list'
 alias brews='brew search'
 alias brewu='brew upgrade'
-alias brewU='brew update && brew upgrade'
+alias brewU='brew update && brew upgrade --all'
 alias brewx='brew remove'
 
 # Homebrew Cask
