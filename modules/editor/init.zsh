b3bf0f394f2530c2325bdf9c5595ed7b21dc3236
David Mohl <me@dave.cx>
2015-09-04 15:29:04 +0900

    added option to remap escape key

--- modules/editor/init.zsh
+++ modules/editor/init.zsh
@@ -247,6 +247,13 @@ bindkey -M vicmd "v" edit-command-line
 bindkey -M vicmd "u" undo
 bindkey -M vicmd "$key_info[Control]R" redo
 
+
+# Allow remapping of command-mode key
+zstyle -s ':prezto:module:editor' escape-remap 'escape_remap'
+if [[ ! -z "$escape_remap" ]]; then
+  bindkey -M viins "$escape_remap" vi-cmd-mode
+fi
+
 if (( $+widgets[history-incremental-pattern-search-backward] )); then
   bindkey -M vicmd "?" history-incremental-pattern-search-backward
   bindkey -M vicmd "/" history-incremental-pattern-search-forward
