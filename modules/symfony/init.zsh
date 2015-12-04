a15b06a5781c78c9933550ffd2aef0c66b1d5d04
longkey1 <longkey1@gmail.com>
2015-12-04 13:20:00 +0900

    Rename symfony2 to symfony

--- modules/symfony/init.zsh
+++ modules/symfony/init.zsh
@@ -14,18 +14,18 @@ _symfony_console () {
   echo "php $(find . -maxdepth 2 -mindepth 1 -name 'console' -type f | head -n 1)"
 }
 
-_symfony2_get_command_list () {
+_symfony_get_command_list () {
    `_symfony_console` --no-ansi | sed "1,/Available commands/d" | awk '/^  ?[a-z]+/ { print $1 }'
 }
 
-_symfony2 () {
-   compadd `_symfony2_get_command_list`
+_symfony () {
+   compadd `_symfony_get_command_list`
 }
 
-compdef _symfony2 '`_symfony_console`'
-compdef _symfony2 'app/console'
-compdef _symfony2 'bin/console'
-compdef _symfony2 sf
+compdef _symfony '`_symfony_console`'
+compdef _symfony 'app/console'
+compdef _symfony 'bin/console'
+compdef _symfony sf
 
 #Alias
 alias sf='`_symfony_console`'
