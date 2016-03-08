b97b3edc1f85da2ed630173be654af30013b7253
Rick Jones <rickjones@Rick-Jones.local>
2016-03-08 16:00:29 +0000

    Git tag aliases

--- modules/git/alias.zsh
+++ modules/git/alias.zsh
@@ -168,6 +168,10 @@ alias gSs='git submodule sync'
 alias gSu='git submodule foreach git pull origin master'
 alias gSx='git-submodule-remove'
 
+# Tag (t)
+alias gt='git tag'
+alias gtl='git tag -l'
+
 # Working Copy (w)
 alias gws='git status --ignore-submodules=${_git_status_ignore_submodules} --short'
 alias gwS='git status --ignore-submodules=${_git_status_ignore_submodules}'
