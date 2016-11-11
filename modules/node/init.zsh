c82d7717352c314f561bd6e712dfce4eebe85918
Eduardo San Martin Morote <posva13@gmail.com>
2016-11-11 18:07:45 +0100

    Remove NVM from zshrc and perf improve node plugin

--- modules/node/init.zsh
+++ modules/node/init.zsh
@@ -10,10 +10,6 @@
 if [[ -s "$HOME/.nvm/nvm.sh" ]]; then
   source "$HOME/.nvm/nvm.sh"
 
-# Load package manager installed NVM into the shell session.
-elif (( $+commands[brew] )) && [[ -d "$(brew --prefix nvm 2>/dev/null)" ]]; then
-  source $(brew --prefix nvm)/nvm.sh
-
 # Return if requirements are not found.
 elif (( ! $+commands[node] )); then
   return 1
