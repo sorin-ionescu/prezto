57bd7c0917ccc75c0a805ea1d3bcaa7e161014d9
J. Morgan Lieberthal <j.morgan.lieberthal@gmail.com>
2016-12-09 10:21:00 -0700

    Update rails plugin

--- modules/rails/init.zsh
+++ modules/rails/init.zsh
@@ -15,6 +15,26 @@ if (( ! $+commands[bundle] )); then
   return 1
 fi
 
+function _rails_command {
+  if [ -e "bin/rails" ]; then
+    bin/rails $@
+  elif type bundle &> /dev/null && [ -e "Gemfile" ]; then
+    bundle exec rails $@
+  else
+    command rails $@
+  fi
+}
+
+function _rake_command {
+  if [ -e "bin/rake" ]; then
+    bin/rake $@
+  elif type bundle &> /dev/null && [ -e "Gemfile" ]; then
+    bundle exec rake $@
+  else
+    command rake $@
+  fi
+}
+
 #
 # Aliases
 #
@@ -27,22 +47,28 @@ alias Rdevlog='tail -f log/development.log'
 alias Rprodlog='tail -f log/production.log'
 alias Rtestlog='tail -f log/test.log'
 
-alias ror='bundle exec rails'
-alias rorc='bundle exec rails console'
-alias rord='bundle exec rails destroy'
-alias rordb='bundle exec rails dbconsole'
-alias rorg='bundle exec rails generate'
-alias rorgm='bundle exec rails generate migration'
+alias rails='_rails_command'
+compdef _rails_command=rails
+
+alias rake='_rake_command'
+compdef _rake_command=rake
+
+alias ror='rails'
+alias rorc='rails console'
+alias rord='rails destroy'
+alias rordb='rails dbconsole'
+alias rorg='rails generate'
+alias rorgm='rails generate migration'
 alias rorl='tail -f "$(ruby-app-root)/log/development.log"'
-alias rorlc='bundle exec rake log:clear'
-alias rorp='bundle exec rails plugin'
-alias rorr='bundle exec rails runner'
-alias rors='bundle exec rails server'
-alias rorsd='bundle exec rails server --debugger'
-alias rorx='bundle exec rails destroy'
+alias rorlc='rake log:clear'
+alias rorp='rails plugin'
+alias rorr='rails runner'
+alias rors='rails server'
+alias rorsd='rails server --debugger'
+alias rorx='rails destroy'
 
 # Rake-specific stuff
-alias rdm='bundle exec rake db:migrate'
+alias rdm='rake db:migrate'
 alias rdbr='rake db:rollback'
 alias rdbc='rake db:create'
 alias rdbs='rake db:seed'
@@ -50,5 +76,5 @@ alias rdbd='rake db:drop'
 alias rdbrs='rake db:reset'
 alias rdbtc='rake db:test:clone'
 alias rdbtp='rake db:test:prepare'
-alias rdbms='bundle exec rake db:migrate:status'
+alias rdbms='rake db:migrate:status'
 alias rdbmtc='rake db:migrate db:test:clone'
