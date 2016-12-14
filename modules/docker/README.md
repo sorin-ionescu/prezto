ee49c46b4ca975f14302860f1857d8f084fd4880
François Vantomme <akarzim@gmail.com>
2016-12-14 11:51:50 +0100

    Feat (docker): add more aliases
    Thanks to @malikoth and @saoula for their proposals

--- modules/docker/README.md
+++ modules/docker/README.md
@@ -13,43 +13,78 @@ Defines [Docker][1] aliases.
   - `dke` Run a command in a running container
   - `dkE` Run an interactive command in a running container
   - `dkh` Show the history of an image
+  - `dki` List images
+  - `dkin` Return low-level information on a container, image or task
   - `dkk` Kill a running container
   - `dkl` Fetch the logs of a container
+  - `dkli` Log in to a Docker registry
+  - `dklo` Log out from a Docker registry
   - `dkn` Manage Docker networks
+  - `dkp` Pause all processes within one or more containers<Paste>
+  - `dkP` Unpause all processes within one or more containers
   - `dkps` List containers
+  - `dkpsa` List all containers (default lists just running)
   - `dkr` Run a command in a new container
+  - `dkR` Run an interactive command in a new container and automatically remove
+  the container when it exits
+  - `dkRe` like `dkR` and set entry point to `/bin/bash`
   - `dkrm` Remove one or more containers
   - `dkrmi` Remove one or more images
+  - `dkrmC` Clean up exited containers
+  - `dkrmI` Clean up dangling images
   - `dks` Start one or more stopped containers
   - `dkS` Restart a container
   - `dkss` Display a live stream of container(s) resource usage statistics
+  - `dksw` Manage Docker Swarm
+  - `dkt` Tag an image into a repository
+  - `dktop` Display the running processes of a container
+  - `dkup` Update configuration of one or more containers
+  - `dkv` Manage Docker volumes
+  - `dkV` Show the Docker version information
   - `dkw` Block until a container stops, then print its exit code<Paste>
   - `dkx` Stop a running container
 
 ### Docker Machine
 
   - `dkm` is short for `docker-machine`
+  - `dkma` Get or set the active machine
+  - `dkmcp` Copy files between machines
+  - `dkme` Set up the environment for the Docker client
+  - `dkmin` Inspect information about a machine
   - `dkmip` Get the IP address of a machine
+  - `dkmk` Kill a machine
   - `dkmls` List machines
+  - `dkmpr` Re-provision existing machines
   - `dkmps` alias for `dkmls`
+  - `dkmrg` Regenerate TLS Certificates for a machine
+  - `dkmrm` Remove a machine
   - `dkms` Start a machine
+  - `dkmsh` Log into or run a command on a machine with SSH
+  - `dkmst` Get the status of a machine
   - `dkmS` Restart a machine
   - `dkmu` Get the URL of a machine
   - `dkmup` Upgrade a machine to the latest version of Docker
+  - `dkmV` Show the Docker Machine version or a machine docker version
   - `dkmx` Stop a machine
 
 ### Docker Compose
 
   - `dkc` is short for `docker-compose`
+  - `dkcb` Build or rebuild services
   - `dkcd` Stop and remove containers, networks, images, and volumes
+  - `dkce` Execute a command in a running container
   - `dkck` Kill containers
   - `dkcl` View output from containers
+  - `dkcp` Pause services
+  - `dkcP` Unpause services
   - `dkcps` List containers
   - `dkcr` Run a one-off command
   - `dkcrm` Remove stopped containers
   - `dkcs` Start services
+  - `dkcsc` Set number of containers for a service
   - `dkcS` Restart services
   - `dkcu` Create and start containers
+  - `dkcV` Show the Docker-Compose version information
   - `dkcx` Stop services
 
 ## Author
--- modules/docker/init.zsh
+++ modules/docker/init.zsh
@@ -28,37 +28,106 @@ alias dkd='docker diff'
 alias dke='docker exec'
 alias dkE='docker exec -it'
 alias dkh='docker history'
+alias dki='docker images'
+alias dkin='docker inspect'
 alias dkk='docker kill'
 alias dkl='docker logs'
+alias dkli='docker login'
+alias dklo='docker logout'
 alias dkn='docker network'
+alias dkp='docker pause'
+alias dkP='docker unpause'
 alias dkps='docker ps'
+alias dkpsa='docker ps -a'
 alias dkr='docker run'
+alias dkR='docker run -it --rm'
+alias dkRe='docker run -it --rm --entrypoint /bin/bash'
 alias dkrm='docker rm'
 alias dkrmi='docker rmi'
+alias dkrn='docker rename'
 alias dks='docker start'
 alias dkS='docker restart'
 alias dkss='docker stats'
+alias dksw='docker swarm'
+alias dkt='docker tag'
+alias dktop='docker top'
+alias dkup='docker update'
+alias dkv='docker volume'
+alias dkV='docker version'
 alias dkw='docker wait'
 alias dkx='docker stop'
 
+# Clean up exited containers
+alias dkrmC='docker rm $(docker ps -a | grep Exited | awk '"'"'{ print $1 }'"'"')'
+
+# Clean up dangling images
+alias dkrmI='docker rmi $(docker images -f dangling=true -q)'
+
+## Non aliased docker commands:
+# commit   -- Create a new image from a container's changes
+# cp       -- Copy files/folders between a container and the local filesystem
+# create   -- Create a new container
+# daemon   -- Enable daemon mode
+# events   -- Get real time events from the server
+# export   -- Export a container's filesystem as a tar archive
+# import   -- Import the contents from a tarball to create a filesystem image
+# info     -- Display system-wide information
+# load     -- Load an image from a tar archive or STDIN
+# node     -- Manage Docker Swarm nodes
+# port     -- List port mappings or a specific mapping for the container
+# pull     -- Pull an image or a repository from a registry
+# push     -- Push an image or a repository to a registry
+# save     -- Save one or more images to a tar archive (streamed to STDOUT by default)
+# search   -- Search the Docker Hub for images
+# service  -- Manage Docker services
+
 alias dkm='docker-machine'
+alias dkma='docker-machine active'
+alias dkmcp='docker-machine scp'
+alias dkmin='docker-machine inspect'
 alias dkmip='docker-machine ip'
+alias dkmk='docker-machine kill'
 alias dkmls='docker-machine ls'
+alias dkmpr='docker-machine provision'
 alias dkmps='docker-machine ps'
+alias dkmrg='docker-machine regenerate-certs'
+alias dkmrm='docker-machine rm'
 alias dkms='docker-machine start'
+alias dkmsh='docker-machine ssh'
+alias dkmst='docker-machine status'
 alias dkmS='docker-machine restart'
 alias dkmu='docker-machine url'
 alias dkmup='docker-machine upgrade'
+alias dkmV='docker-machine version'
 alias dkmx='docker-machine stop'
 
+# Non aliased docker-machine commands:
+# config  -- Print the connection config for machine
+# create  -- Create a machine
+
 alias dkc='docker-compose'
+alias dkcb='docker-compose build'
 alias dkcd='docker-compose down'
+alias dkce='docker-compose exec'
 alias dkck='docker-compose kill'
 alias dkcl='docker-compose logs'
+alias dkp='docker-compose pause'
+alias dkP='docker-compose unpause'
 alias dkcps='docker-compose ps'
 alias dkcr='docker-compose run'
 alias dkcrm='docker-compose rm'
 alias dkcs='docker-compose start'
+alias dkcsc='docker-compose scale'
 alias dkcS='docker-compose restart'
 alias dkcu='docker-compose up'
+alias dkcV='docker-compose version'
 alias dkcx='docker-compose stop'
+
+## Non aliased docker-compose commands:
+# bundle  -- Generate a Docker bundle from the Compose file
+# config  -- Validate and view the compose file
+# create  -- Create services
+# events  -- Receive real time events from containers
+# port    -- Print the public port for a port binding
+# pull    -- Pull service images
+# push    -- Push service images
