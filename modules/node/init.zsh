8dafaae5a43d3019d74a74c7d57430c5db689d5d
Griffin Yourick <gryphon92@gmail.com>
2017-04-10 14:15:08 -0400

    Use patterns from ruby module for initializing nodenv

--- modules/node/init.zsh
+++ modules/node/init.zsh
@@ -16,11 +16,12 @@ elif (( $+commands[brew] )) && [[ -d "$(brew --prefix nvm 2>/dev/null)" ]]; then
 
 # Load manually installed nodenv into the shell session.
 elif [[ -s "$HOME/.nodenv/bin/nodenv" ]]; then
-  eval "$($HOME/.nodenv/bin/nodenv init -)"
+  path=("$HOME/.rbenv/bin $path")
+  eval "$(nodenv init - --no-rehash zsh)"
 
 # Load package manager installed nodenv into the shell session.
-elif (( $+commands[brew] )) && [[ -d "$(brew --prefix nodenv 2>/dev/null)" ]]; then
-  eval "$($(brew --prefix nodenv)/bin/nodenv init -)"
+elif (( $+commands[nodenv] )); then
+  eval "$(nodenv init - --no-rehash zsh)"
 
 # Return if requirements are not found.
 elif (( ! $+commands[node] )); then
