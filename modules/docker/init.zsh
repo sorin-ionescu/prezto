429900eb6ed81d2f7fcbeba7d40439bceff15a88
François Vantomme <akarzim@gmail.com>
2017-04-13 11:25:02 +0200

    Style (docker): use two-space tabs

--- modules/docker/init.zsh
+++ modules/docker/init.zsh
@@ -7,7 +7,7 @@
 
 # Return if requirements are not found.
 if (( ! $+commands[docker] )); then
-    return 1
+  return 1
 fi
 
 #
@@ -16,41 +16,41 @@ fi
 
 # Set Docker Machine environment
 function dkme {
-    if (( ! $+commands[docker-machine] )); then
-        return 1
-    fi
+  if (( ! $+commands[docker-machine] )); then
+    return 1
+  fi
 
-    eval $(docker-machine env $1)
+  eval $(docker-machine env $1)
 }
 
 # Set Docker Machine default machine
 function dkmd {
-    if (( ! $+commands[docker-machine] )); then
-        return 1
-    fi
+  if (( ! $+commands[docker-machine] )); then
+    return 1
+  fi
 
-    pushd ~/.docker/machine/machines
+  pushd ~/.docker/machine/machines
 
-    if [[ ! -d $1 ]]; then
-        echo "Docker machine '$1' does not exists. Abort."
-        popd
-        return 1
-    fi
-
-    if [[ -L default ]]; then
-        eval $(rm -f default)
-    elif [[ -d default ]]; then
-        echo "A default manchine already exists. Abort."
-        popd
-        return 1
-    elif [[ -e default ]]; then
-        echo "A file named 'default' already exists. Abort."
-        popd
-        return 1
-    fi
-
-    eval $(ln -s $1 default)
+  if [[ ! -d $1 ]]; then
+    echo "Docker machine '$1' does not exists. Abort."
     popd
+    return 1
+  fi
+
+  if [[ -L default ]]; then
+    eval $(rm -f default)
+  elif [[ -d default ]]; then
+    echo "A default manchine already exists. Abort."
+    popd
+    return 1
+  elif [[ -e default ]]; then
+    echo "A file named 'default' already exists. Abort."
+    popd
+    return 1
+  fi
+
+  eval $(ln -s $1 default)
+  popd
 }
 
 #
