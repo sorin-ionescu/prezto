1f21fc064bf94d736952fa6e735275cb95fe078d
Matthew Crenshaw <sgt@squig.gs>
2017-05-16 13:47:47 -0400

    using local variables, added git check, made auto workon opt-in instead of opt-out

--- modules/python/init.zsh
+++ modules/python/init.zsh
@@ -35,44 +35,49 @@ if (( ! $+commands[python] && ! $+commands[pyenv] )); then
 fi
 
 function _python-workon-cwd {
-  if [ ! $WORKON_CWD ]; then
-    WORKON_CWD=1
-    # Look for a proper PROJECT_ROOT path
-    PROJECT_ROOT=$(pwd)
-    while [[ "$PROJECT_ROOT" != "/" && ! -e "$PROJECT_ROOT/.venv" ]]; do
-      PROJECT_ROOT=${PROJECT_ROOT:A:h}
-    done
-
-    if [[ "$PROJECT_ROOT" == "/" ]]; then
-      PROJECT_ROOT="."
-    fi
-    # Check for virtualenv name override
-    if [[ -f "$PROJECT_ROOT/.venv" ]]; then
-      ENV_NAME=$(cat "$PROJECT_ROOT/.venv")
-    elif [[ -f "$PROJECT_ROOT/.venv/bin/activate" ]]; then
-      ENV_NAME="$PROJECT_ROOT/.venv"
-    else
-      ENV_NAME=""
-    fi
-    if [[ -n $CD_VIRTUAL_ENV && -n $VIRTUAL_ENV ]]; then
-      # We've just left the repo, deactivate the environment
-      # Note: this only happens if the virtualenv was activated automatically
-      deactivate && unset CD_VIRTUAL_ENV
-    fi
-    if [[ "$ENV_NAME" != "" ]]; then
-      # Activate the environment only if it is not already active
-      if [[ "$VIRTUAL_ENV" != "$WORKON_HOME/$ENV_NAME" ]]; then
-        if [[ -e "$WORKON_HOME/$ENV_NAME/bin/activate" ]]; then
-          workon "$ENV_NAME" && export CD_VIRTUAL_ENV="$ENV_NAME"
-        elif [[ -e "$ENV_NAME/bin/activate" ]]; then
-          source $ENV_NAME/bin/activate && export CD_VIRTUAL_ENV="$ENV_NAME"
+    if [[ -z "$WORKON_CWD" ]]; then
+      local WORKON_CWD=1
+      # Check if this is a Git repo
+      local GIT_REPO_ROOT=""
+      local GIT_TOPLEVEL="$(git rev-parse --show-toplevel 2> /dev/null)"
+      if [[ $? == 0 ]]; then
+        GIT_REPO_ROOT="$GIT_TOPLEVEL"
+      fi
+      # Get absolute path, resolving symlinks
+      local PROJECT_ROOT="${PWD:A}"
+      while [[ "$PROJECT_ROOT" != "/" && ! -e "$PROJECT_ROOT/.venv" \
+               && ! -d "$PROJECT_ROOT/.git"  && "$PROJECT_ROOT" != "$GIT_REPO_ROOT" ]]; do
+        PROJECT_ROOT="${PROJECT_ROOT:h}"
+      done
+      if [[ "$PROJECT_ROOT" == "/" ]]; then
+        PROJECT_ROOT="."
+      fi
+      # Check for virtualenv name override
+      local ENV_NAME=""
+      if [[ -f "$PROJECT_ROOT/.venv" ]]; then
+        ENV_NAME="$(cat "$PROJECT_ROOT/.venv")"
+      elif [[ -f "$PROJECT_ROOT/.venv/bin/activate" ]];then
+        ENV_NAME="$PROJECT_ROOT/.venv"
+      elif [[ "$PROJECT_ROOT" != "." ]]; then
+        ENV_NAME="${PROJECT_ROOT:t}"
+      fi
+      if [[ -n $CD_VIRTUAL_ENV && -n $VIRTUAL_ENV ]]; then
+        # We've just left the repo, deactivate the environment
+        # Note: this only happens if the virtualenv was activated automatically
+        deactivate && unset CD_VIRTUAL_ENV
+      fi
+      if [[ "$ENV_NAME" != "" ]]; then
+        # Activate the environment only if it is not already active
+        if [[ "$VIRTUAL_ENV" != "$WORKON_HOME/$ENV_NAME" ]]; then
+          if [[ -e "$WORKON_HOME/$ENV_NAME/bin/activate" ]]; then
+            workon "$ENV_NAME" && export CD_VIRTUAL_ENV="$ENV_NAME"
+          elif [[ -e "$ENV_NAME/bin/activate" ]]; then
+            source $ENV_NAME/bin/activate && export CD_VIRTUAL_ENV="$ENV_NAME"
+          fi
         fi
       fi
     fi
-    unset PROJECT_ROOT
-    unset WORKON_CWD
-  fi
-}
+  }
 
 # Load virtualenvwrapper into the shell session, unless requested not to
 if zstyle -T ':prezto:module:python' skip-virtualenvwrapper-init; then
@@ -84,8 +89,7 @@ if zstyle -T ':prezto:module:python' skip-virtualenvwrapper-init; then
 
   source "$commands[virtualenvwrapper.sh]"
 
-  zstyle -t ':prezto:module:python' disable-virtualenv-auto-workon-cwd
-  if (( $? )); then
+  if zstyle -t ':prezto:module:python' enable-virtualenv-auto-workon-cwd 'yes'; then
     # Auto workon when changing directory
     add-zsh-hook chpwd _python-workon-cwd
   fi
