sorin-ionescu/prezto

commit 90071d3ad062c4969b1d2e8512e9238524b90981

utility: fix completion for rsync and scp

modules/utility/init.zsh
@@ -41,8 +41,8 @@ alias ftp='noglob ftp'
 alias history='noglob history'
 alias locate='noglob locate'
 alias rake='noglob rake'
-alias rsync='noglob rsync_scp_wrap rsync'
-alias scp='noglob rsync_scp_wrap scp'
+alias rsync='noglob rsync_wrap'
+alias scp='noglob scp_wrap'
 # This function wraps rsync and scp so that remote paths are not globbed
 # but local paths are globbed. This is because the programs have their own
 # globbing for remote paths. The wrap function globs args starting in / and ./
@@ -61,6 +61,15 @@ function rsync_scp_wrap {
   esac; done
   command $cmd "${(@)args}"
 }
+function rsync_wrap {
+  rsync_scp_wrap "rsync" "$@"
+}
+function scp_wrap {
+  rsync_scp_wrap "scp" "$@"
+}
+compdef _rsync rsync_wrap
+compdef _scp scp_wrap
+
 alias sftp='noglob sftp'
 
 # Define general aliases.

commit 205909a37cefa9ed895359f4c9eebf55c12a3e57

[utility] Remove premature call to `compdef`
This is a temporary fix to work-around a minor regression in 90071d3.

modules/utility/init.zsh
@@ -67,8 +67,6 @@ function rsync_wrap {
 function scp_wrap {
   rsync_scp_wrap "scp" "$@"
 }
-compdef _rsync rsync_wrap
-compdef _scp scp_wrap
 
 alias sftp='noglob sftp'
 