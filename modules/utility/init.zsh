608f291ad3c5955f4b6c413adbef2b0a820ef65e
Indrajit Raychaudhuri <irc@indrajit.com>
2017-07-20 23:11:00 -0500

    [utility] Reuse interactive aliases when 'safe-ops' is set
    When `safe-ops` is set, we can reuse the aliases that are already available.

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -89,10 +89,10 @@ alias mvi="${aliases[mv]:-mv} -i"
 alias cpi="${aliases[cp]:-cp} -i"
 alias lni="${aliases[ln]:-ln} -i"
 if zstyle -T ':prezto:module:utility' safe-ops; then
-  alias rm="${aliases[rm]:-rm} -i"
-  alias mv="${aliases[mv]:-mv} -i"
-  alias cp="${aliases[cp]:-cp} -i"
-  alias ln="${aliases[ln]:-ln} -i"
+  alias rm='rmi'
+  alias mv='mvi'
+  alias cp='cpi'
+  alias ln='lni'
 fi
 
 # ls
