b5d09e2da794086ab5715f3fdda2e9c4b6dd3d9d
Parth Laxmikant Kolekar <ParthKolekar@users.noreply.github.com>
2017-10-20 23:08:48 +0530

    Lower requirements down to 4.3.11 again (#1491)
    There was an update with the syntax-highlighting module which fixed compatibility with 4.3.11.

--- init.zsh
+++ init.zsh
@@ -10,7 +10,7 @@
 #
 
 # Check for the minimum supported version.
-min_zsh_version='4.3.17'
+min_zsh_version='4.3.11'
 if ! autoload -Uz is-at-least || ! is-at-least "$min_zsh_version"; then
   printf "prezto: old shell detected, minimum required: %s\n" "$min_zsh_version" >&2
   return 1
