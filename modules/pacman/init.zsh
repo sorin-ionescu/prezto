17b325e59cdfccbe7226ce4025f5b3d8430d5cae
Kaleb Elwert <belak@coded.io>
2018-01-24 13:47:20 -0800

    pacman: Simplify support for AUR helpers
    There are a number of things happening here.
    
    - Extra support for yaourt has been removed
    - Docs have been updated to explicitly call out that AUR helpers are not
      officially supported
    - aurutils has been suggested to make common operations easier
    - A utility function called aurget (similar to aurfetch from aurutils)
      has been added to make cloning AUR repos easier.
    
    Fixes #1531

--- modules/pacman/init.zsh
+++ modules/pacman/init.zsh
@@ -23,10 +23,6 @@ zstyle -s ':prezto:module:pacman' frontend '_pacman_frontend'
 
 if (( $+commands[$_pacman_frontend] )); then
   alias pacman="$_pacman_frontend"
-
-  if [[ -s "${0:h}/${_pacman_frontend}.zsh" ]]; then
-    source "${0:h}/${_pacman_frontend}.zsh"
-  fi
 else
   _pacman_frontend='pacman'
   _pacman_sudo='sudo '
@@ -80,5 +76,13 @@ fi
 # Synchronizes the local package database against the repositories then
 # upgrades outdated packages.
 alias pacU="${_pacman_sudo}${_pacman_frontend} --sync --refresh --sysupgrade"
+
+function aurget {
+  local target_dir="$1"
+  if [[ -n "$2" ]]; then
+    target_dir="$2"
+  fi
+  git clone "https://aur.archlinux.org/$1" "$target_dir"
+}
 
 unset _pacman_{frontend,sudo}
