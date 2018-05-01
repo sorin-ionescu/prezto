6e179f24ab606f5c38d12b333771b4ba6b087eba
Casey McGinty <casey.mcginty@gmail.com>
2018-04-30 22:22:10 -0700

    Export VIRTUAL_ENV_DISABLE_PROMPT when enabling virutalenv
    Adding `export` ensures the define is available in the shell so pyenv
    or virtualenvwrapper do not duplicate the virtualenv name on the
    prompt.

--- modules/python/init.zsh
+++ modules/python/init.zsh
@@ -94,7 +94,7 @@ if (( $+VIRTUALENVWRAPPER_VIRTUALENV || $+commands[virtualenv] )) && \
   export WORKON_HOME="${WORKON_HOME:-$HOME/.virtualenvs}"
 
   # Disable the virtualenv prompt.
-  VIRTUAL_ENV_DISABLE_PROMPT=1
+  export VIRTUAL_ENV_DISABLE_PROMPT=1
 
   # Create a sorted array of available virtualenv related 'pyenv' commands to
   # look for plugins of interest. Scanning shell '$path' isn't enough as they
