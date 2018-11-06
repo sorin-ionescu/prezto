d49333360e9f98c9e87d63db305440a06865e821
Rui Coelho <rui.coelho@miniclip.com>
2018-11-06 14:02:07 +0000

    Also lookup on ~/.ssh/config.d/* files when doing ssh host complete

--- modules/completion/init.zsh
+++ modules/completion/init.zsh
@@ -109,7 +109,7 @@ zstyle -a ':prezto:module:completion:*:hosts' etc-host-ignores '_etc_host_ignore
 zstyle -e ':completion:*:hosts' hosts 'reply=(
   ${=${=${=${${(f)"$(cat {/etc/ssh/ssh_,~/.ssh/}known_hosts(|2)(N) 2> /dev/null)"}%%[#| ]*}//\]:[0-9]*/ }//,/ }//\[/ }
   ${=${(f)"$(cat /etc/hosts(|)(N) <<(ypcat hosts 2> /dev/null))"}%%(\#${_etc_host_ignores:+|${(j:|:)~_etc_host_ignores}})*}
-  ${=${${${${(@M)${(f)"$(cat ~/.ssh/config 2> /dev/null)"}:#Host *}#Host }:#*\**}:#*\?*}}
+  ${=${${${${(@M)${(f)"$(cat ~/.ssh/config ~/.ssh/config.d/* 2> /dev/null)"}:#Host *}#Host }:#*\**}:#*\?*}}
 )'
 
 # Don't complete uninteresting users...
