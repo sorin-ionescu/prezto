3093f1b966985c834fb698dabbe9a0af805ef940
Indrajit Raychaudhuri <irc@indrajit.com>
2018-12-14 15:40:59 -0600

    homebrew: Load 'HOMEBREW_' prefixed variables only
    Load 'HOMEBREW_' prefixed variables only. Avoid loading 'PATH'
    related variables as they are already handled in standard zsh 
    configuration.

--- modules/homebrew/init.zsh
+++ modules/homebrew/init.zsh
@@ -14,10 +14,11 @@ fi
 # Variables
 #
 
-# Load standard Homebrew shellenv into the shell session.
-# `brew shellenv` is relatively new, guard for legacy Homebrew.
+# Load standard Homebrew shellenv into the shell session. 
+# Load 'HOMEBREW_' prefixed variables only. Avoid loading 'PATH' related
+# variables as they are already handled in standard zsh configuration.
 if (( $+commands[brew] )); then
-  eval "$(brew shellenv 2> /dev/null)"
+  eval "${(@M)${(f)"$(brew shellenv 2> /dev/null)"}:#export HOMEBREW*}"
 fi
 
 #
