4abbc5572149baa6a5e7e38393a4b2006f01024f
Yutian Li <hotpxless@gmail.com>
2019-01-04 13:05:37 -0500

    remove mutually exclusive option

--- modules/history/init.zsh
+++ modules/history/init.zsh
@@ -12,7 +12,6 @@
 
 setopt BANG_HIST                 # Treat the '!' character specially during expansion.
 setopt EXTENDED_HISTORY          # Write the history file in the ':start:elapsed;command' format.
-setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
 setopt SHARE_HISTORY             # Share history between all sessions.
 setopt HIST_EXPIRE_DUPS_FIRST    # Expire a duplicate event first when trimming history.
 setopt HIST_IGNORE_DUPS          # Do not record an event that was just recorded again.
