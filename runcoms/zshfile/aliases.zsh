495fc8e1f65238b0547052e98b1e79e2ba32746f
kokeiro <kokeiro@kokeiro-deps.local>
2019-03-18 23:04:47 +0900

    add zshfile and aliases.zsh

---
+++ runcoms/zshfile/aliases.zsh
@@ -0,0 +1,11 @@
+# git
+alias gstas='git status'
+alias gdif='git diff'
+alias glog='git log'
+alias gshow='git show '
+alias gadd='git add '
+alias gcomi='git commit '
+alias gcomm='git commit -m '
+alias gbrah='git branch '
+alias gcheo='git checkout '
+alias gpull='git pull '
