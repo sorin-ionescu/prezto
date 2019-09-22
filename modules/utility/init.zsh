2873885d37e362ae9be8ff4cb93ae36739ac0067
Kaleb Elwert <belak@coded.io>
2019-09-21 17:29:18 -0700

    utility: fix safe-ops commands
    Fixes #1699

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -66,10 +66,10 @@ alias mvi="${aliases[mv]:-mv} -i"
 alias cpi="${aliases[cp]:-cp} -i"
 alias lni="${aliases[ln]:-ln} -i"
 if zstyle -T ':prezto:module:utility' safe-ops; then
-  alias rm='rmi'
-  alias mv='mvi'
-  alias cp='cpi'
-  alias ln='lni'
+  alias rm="${aliases[rm]:-rm} -i"
+  alias mv="${aliases[mv]:-mv} -i"
+  alias cp="${aliases[cp]:-cp} -i"
+  alias ln="${aliases[ln]:-ln} -i"
 fi
 
 # ls
