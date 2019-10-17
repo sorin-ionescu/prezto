39f82a047841087073d4bdd733d214d41bbbd44a
lambdanerd <hello.lambda.nerd@gmail.com>
2019-10-17 01:55:40 -0500

    homebrew: add alias `brewL`

--- modules/homebrew/init.zsh
+++ modules/homebrew/init.zsh
@@ -14,7 +14,7 @@ fi
 # Variables
 #
 
-# Load standard Homebrew shellenv into the shell session. 
+# Load standard Homebrew shellenv into the shell session.
 # Load 'HOMEBREW_' prefixed variables only. Avoid loading 'PATH' related
 # variables as they are already handled in standard zsh configuration.
 if (( $+commands[brew] )); then
@@ -28,6 +28,7 @@ fi
 # Homebrew
 alias brewc='brew cleanup'
 alias brewi='brew install'
+alias brewL='brew leaves'
 alias brewl='brew list'
 alias brewo='brew outdated'
 alias brews='brew search'
