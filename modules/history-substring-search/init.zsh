8cc76adaf103b9105373be94c84ab2a946bf3757
Amyn Bennamane <amynb@gentrack.com>
2020-05-02 17:00:22 +0100

    Do not source HSS plugin if already loaded

--- modules/history-substring-search/init.zsh
+++ modules/history-substring-search/init.zsh
@@ -10,5 +10,52 @@
 pmodload 'editor'
 
 # Source module files.
-source "${0:h}/external/zsh-history-substring-search.zsh" || return 1
-source "${0:h}/configure.zsh" || return 1
+if (( ! $+functions[history-substring-search-up] )); then
+  source "${0:h}/external/zsh-history-substring-search.zsh" || return 1
+fi
+
+#
+# Search
+#
+
+zstyle -s ':prezto:module:history-substring-search:color' found \
+  'HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND' \
+    || HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='bg=magenta,fg=white,bold'
+
+zstyle -s ':prezto:module:history-substring-search:color' not-found \
+  'HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND' \
+    || HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='bg=red,fg=white,bold'
+
+zstyle -s ':prezto:module:history-substring-search' globbing-flags \
+  'HISTORY_SUBSTRING_SEARCH_GLOBBING_FLAGS' \
+    || HISTORY_SUBSTRING_SEARCH_GLOBBING_FLAGS='i'
+
+if zstyle -t ':prezto:module:history-substring-search' case-sensitive; then
+  HISTORY_SUBSTRING_SEARCH_GLOBBING_FLAGS="${HISTORY_SUBSTRING_SEARCH_GLOBBING_FLAGS//i}"
+fi
+
+if ! zstyle -t ':prezto:module:history-substring-search' color; then
+  unset HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_{FOUND,NOT_FOUND}
+fi
+
+#
+# Key Bindings
+#
+
+if [[ -n "$key_info" ]]; then
+  # Emacs
+  bindkey -M emacs "$key_info[Control]P" history-substring-search-up
+  bindkey -M emacs "$key_info[Control]N" history-substring-search-down
+
+  # Vi
+  bindkey -M vicmd "k" history-substring-search-up
+  bindkey -M vicmd "j" history-substring-search-down
+
+  # Emacs and Vi
+  for keymap in 'emacs' 'viins'; do
+    bindkey -M "$keymap" "$key_info[Up]" history-substring-search-up
+    bindkey -M "$keymap" "$key_info[Down]" history-substring-search-down
+  done
+
+  unset keymap
+fi
