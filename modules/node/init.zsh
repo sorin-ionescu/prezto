aad323af60b3d21b1e0a34e94aff0e79af3cf6cb
Texas Toland <texastoland@gmail.com>
2020-06-05 05:53:44 +0900

    Make .zcomp* location configurable (#1842)

--- modules/node/init.zsh
+++ modules/node/init.zsh
@@ -1,65 +1,58 @@
 #
-# Configures Node local installation, loads version managers, and defines
-# variables and aliases.
+# Loads the Node Version Manager and enables npm completion.
 #
 # Authors:
 #   Sorin Ionescu <sorin.ionescu@gmail.com>
 #   Zeh Rizzatti <zehrizzatti@gmail.com>
-#   Indrajit Raychaudhuri <irc@indrajit.com>
 #
 
-# Possible lookup locations for manually installed nodenv and nvm.
-local_nodenv_paths=({$NODENV_ROOT,{$XDG_CONFIG_HOME/,$HOME/.}nodenv}/bin/nodenv(N))
-local_nvm_paths=({$NVM_DIR,{$XDG_CONFIG_HOME/,$HOME/.}nvm}/nvm.sh(N))
+# Load manually installed NVM into the shell session.
+if [[ -s "${NVM_DIR:=$HOME/.nvm}/nvm.sh" ]]; then
+  source "${NVM_DIR}/nvm.sh"
 
-# Load manually installed or package manager installed nodenv into the shell
-# session.
-if (( $#local_nodenv_paths || $+commands[nodenv] )); then
+# Load package manager installed NVM into the shell session.
+elif (( $+commands[brew] )) && \
+  [[ -d "${nvm_prefix::="$(brew --prefix 2> /dev/null)"/opt/nvm}" ]]; then
+  source "$(brew --prefix nvm)/nvm.sh"
+  unset nvm_prefix
 
-  # Ensure manually installed nodenv is added to path when present.
-  [[ -s $local_nodenv_paths[1] ]] && path=($local_nodenv_paths[1]:h $path)
+# Load manually installed nodenv into the shell session.
+elif [[ -s "${NODENV_ROOT:=$HOME/.nodenv}/bin/nodenv" ]]; then
+  path=("${NODENV_ROOT}/bin" $path)
+  eval "$(nodenv init - --no-rehash zsh)"
 
-  eval "$(nodenv init - zsh)"
-
-# Load manually installed nvm into the shell session.
-elif (( $#local_nvm_paths )); then
-  source "$local_nvm_paths[1]" --no-use
-
-# Load package manager installed nvm into the shell session.
-elif (( $+commands[brew] )) \
-      && [[ -d "${nvm_path::="$(brew --prefix 2> /dev/null)"/opt/nvm}" ]]; then
-  source "$nvm_path/nvm.sh" --no-use
-fi
-
-unset local_n{odenv,vm}_paths nvm_path
+# Load package manager installed nodenv into the shell session.
+elif (( $+commands[nodenv] )); then
+  eval "$(nodenv init - --no-rehash zsh)"
 
 # Return if requirements are not found.
-if (( ! $+commands[node] && ! $#functions[(i)n(odenv|vm)] )); then
+elif (( ! $+commands[node] )); then
   return 1
 fi
 
-#
-# Variables
-#
+# Load NPM and known helper completions.
+typeset -A compl_commands=(
+  npm   'npm completion'
+  grunt 'grunt --completion=zsh'
+  gulp  'gulp --completion=zsh'
+)
 
-N_PREFIX="${XDG_CONFIG_HOME:-$HOME/.config}/n"  # The path to 'n' cache.
+for compl_command in "${(k)compl_commands[@]}"; do
+  if (( $+commands[$compl_command] )); then
+    cache_file="${XDG_CACHE_HOME:-$HOME/.cache}/prezto/$compl_command-cache.zsh"
 
-#
-# Aliases
-#
+    # Completion commands are slow; cache their output if old or missing.
+    if [[ "$commands[$compl_command]" -nt "$cache_file" \
+          || "${ZDOTDIR:-$HOME}/.zpreztorc" -nt "$cache_file" \
+          || ! -s "$cache_file" ]]; then
+      mkdir -p "$cache_file:h"
+      command ${=compl_commands[$compl_command]} >! "$cache_file" 2> /dev/null
+    fi
 
-# npm
-alias npmi='npm install'
-alias npml='npm list'
-alias npmo='npm outdated'
-alias npmp='npm publish'
-alias npmP='npm prune'
-alias npmr='npm run'
-alias npms='npm search'
-alias npmt='npm test'
-alias npmu='npm update'
-alias npmx='npm uninstall'
+    source "$cache_file"
 
-alias npmci='npm ci'
-alias npmcit='npm cit'
-alias npmit='npm it'
+    unset cache_file
+  fi
+done
+
+unset compl_command{s,}
