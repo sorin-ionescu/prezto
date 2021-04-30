f651140f2cfcbcf029715ec6f0f970d2c7d0ef7d
Indrajit Raychaudhuri <irc@indrajit.com>
2021-04-30 17:49:32 -0500

    completion: Add note about keep zcompdump younger than cache time

--- modules/completion/init.zsh
+++ modules/completion/init.zsh
@@ -24,7 +24,7 @@ setopt PATH_DIRS           # Perform path search even on command names with slas
 setopt AUTO_MENU           # Show completion menu on a successive tab press.
 setopt AUTO_LIST           # Automatically list choices on ambiguous completion.
 setopt AUTO_PARAM_SLASH    # If completed parameter is a directory, add a trailing slash.
-setopt EXTENDED_GLOB       # Needed for file modification glob modifiers with compinit
+setopt EXTENDED_GLOB       # Needed for file modification glob modifiers with compinit.
 unsetopt MENU_COMPLETE     # Do not autoselect the first completion entry.
 unsetopt FLOW_CONTROL      # Disable start/stop characters in shell editor.
 
@@ -40,6 +40,7 @@ if [[ $_comp_path(#qNmh-20) ]]; then
 else
   mkdir -p "$_comp_path:h"
   compinit -i -d "$_comp_path"
+  # Keep $_comp_path younger than cache time even if it isn't regenerated.
   touch "$_comp_path"
 fi
 unset _comp_path
