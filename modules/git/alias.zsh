b3b7e0f2ce210a3fd0780f9e7324ef61987512e4
hidekuro <hidekuro@gmail.com>
2021-05-17 17:44:02 +0900

    improve gSu
    Change method to `update` instead of `foreach`.

--- modules/git/alias.zsh
+++ modules/git/alias.zsh
@@ -253,7 +253,7 @@ if ! zstyle -t ':prezto:module:git:alias' skip 'yes'; then
   alias gSl='git submodule status'
   alias gSm='git-submodule-move'
   alias gSs='git submodule sync'
-  alias gSu='git submodule foreach git pull'
+  alias gSu='git submodule update --remote --recursive'
   alias gSx='git-submodule-remove'
 
   # Tag (t)
