afb466c87131cfb6606836fde06e06246a6e1d81
hidekuro <hidekuro@gmail.com>
2021-05-17 13:51:27 +0900

    update gSu
    Safe for branches not named "master".
    
    And with this method, we can add any remote and branch name after it such as "origin master" or "upstream main".

--- modules/git/alias.zsh
+++ modules/git/alias.zsh
@@ -253,7 +253,7 @@ if ! zstyle -t ':prezto:module:git:alias' skip 'yes'; then
   alias gSl='git submodule status'
   alias gSm='git-submodule-move'
   alias gSs='git submodule sync'
-  alias gSu='git submodule foreach git pull origin master'
+  alias gSu='git submodule foreach git pull'
   alias gSx='git-submodule-remove'
 
   # Tag (t)
