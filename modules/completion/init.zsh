c8561b3259c26aacde0a5d6ef4eb32bbf246053b
Indrajit Raychaudhuri <irc@indrajit.com>
2021-05-25 15:18:54 -0500

    completion: Avoid double-quoting LS_COLORS for completion
    Double-quoting `LS_COLORS` prevents it from loading in environment,
    which isn't what we want.

--- modules/completion/init.zsh
+++ modules/completion/init.zsh
@@ -40,7 +40,7 @@ unsetopt FLOW_CONTROL      # Disable start/stop characters in shell editor.
 #
 
 # Standard style used by default for 'list-colors'
-LS_COLORS="${LS_COLORS:-'di=34:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'}"
+LS_COLORS=${LS_COLORS:-'di=34:ln=35:so=32:pi=33:ex=31:bd=36;01:cd=33;01:su=31;40;07:sg=36;40;07:tw=32;40;07:ow=33;40;07:'}
 
 #
 # Initialization
