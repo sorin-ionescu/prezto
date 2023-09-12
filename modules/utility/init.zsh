82f5c2c22a1cb5851e81fdbb2b665f250fd032e6
Indrajit Raychaudhuri <irc@indrajit.com>
2023-09-11 21:51:45 -0500

    utility: Detect lsd and honor its GNU ls compatibility
    lsd (LSDeluxe) is a modern GNU compatibile alternative to ls. Detect if
    it is aliased to ls and honor its GNU ls compatibility.

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -76,7 +76,7 @@ if zstyle -T ':prezto:module:utility' safe-ops; then
 fi
 
 # ls
-if [[ ${(@M)${(f)"$(ls --version 2>&1)"}:#*GNU *} ]]; then
+if [[ ${(@M)${(f)"$(ls --version 2>&1)"}:#*(GNU|lsd) *} ]]; then
   # GNU Core Utilities
 
   if zstyle -T ':prezto:module:utility:ls' dirs-first; then
