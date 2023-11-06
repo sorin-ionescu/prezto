855e439c04b7d1d61f669b1f4e194faf58127e47
Abizer Lokhandwala <abizer@abizer.me>
2023-11-06 12:35:24 -0800

    remove alias sl='ls'

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -122,7 +122,6 @@ alias lk='ll -Sr'        # Lists sorted by size, largest last.
 alias lt='ll -tr'        # Lists sorted by date, most recent last.
 alias lc='lt -c'         # Lists sorted by date, most recent last, shows change time.
 alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
-alias sl='ls'            # Correction for common spelling error.
 
 if [[ ${(@M)${(f)"$(ls --version 2>&1)"}:#*GNU *} ]]; then
   alias lx='ll -XB'      # Lists sorted by extension (GNU only).
