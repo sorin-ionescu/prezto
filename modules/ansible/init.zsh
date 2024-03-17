4144cefbb5c76126857a9aa7c22f1e0d896b4927
jcox <200446+jeffcox@users.noreply.github.com>
2024-03-17 09:54:03 -0700

    Clean and document ansible module

--- modules/ansible/init.zsh
+++ modules/ansible/init.zsh
@@ -1,28 +1,7 @@
 #! /usr/bin/env zsh
 
-# Don't check for ansible, we might not have it (yet)
-# if (( ! ${+commands[ansible]} )); then
-#   return 1
-# fi
-
-alias ap=ansible-playbook
-alias galactus=ansible-galaxy
-
-if (( ${+commands[ansible-lint]} )); then
-	alias al=ansible-lint
-fi
-
-if (( ${+commands[awx]} )); then
-# Optional config for awxkit
-# To make use of this add your Tower/AWX token to your login keychain like this:
-# security add-generic-password -a $USER -s awx_token -w SomeAPItoken login.keychain
-	if is-darwin; then
-		export CONTROLLER_TOKEN=$(security find-generic-password -a $USER -s awx_token -w login.keychain)
-	fi
-fi
-
-# Similarly, ARA should be handled by direnv now
-# if (( ${+commands[ara]} )); then
-# # Reasonably modern ara module installation, which implies callback plugin availability
-# 	export ANSIBLE_CALLBACK_PLUGINS="$(python3 -m ara.setup.callback_plugins)"
-# fi
+alias an='ansible-navigator'
+alias ap='ansible-playbook'
+alias apv='ansible-playbook -vvv'
+alias galactus='ansible-galaxy'
+alias al='ansible-lint'
--- modules/ansible/readme.md
+++ modules/ansible/readme.md
@@ -1,2 +1,12 @@
+# Ansible aliases
 
-A quick and dirty module for Ansible to save my time and fingers
+Sets aliases for common ansible related commands.
+
+Assumes ansible will exist, probably from direnv.
+
+## Aliases
+- `al` an alias for 'ansible-lint'
+- `an` an alias for 'ansible-navigator'
+- `ap` an alias for 'ansible-playbook'
+- `apv` an alias for 'ansible-playbook -vvv'
+- `galactus` an alias for 'ansible-galaxy'
