2d86c076c3a5097a229bf8898976f84f2af8f7c2
jcox <jcox@prometheus.localdomain>
2024-03-17 08:55:11 -0700

    Update runcoms and ansible module

--- modules/ansible/init.zsh
+++ modules/ansible/init.zsh
@@ -1,9 +1,9 @@
 #! /usr/bin/env zsh
 
-# Check for ansible
-if (( ! ${+commands[ansible]} )); then
-  return 1
-fi
+# Don't check for ansible, we might not have it (yet)
+# if (( ! ${+commands[ansible]} )); then
+#   return 1
+# fi
 
 alias ap=ansible-playbook
 alias galactus=ansible-galaxy
@@ -15,13 +15,14 @@ fi
 if (( ${+commands[awx]} )); then
 # Optional config for awxkit
 # To make use of this add your Tower/AWX token to your login keychain like this:
-# security add-generic-password -a $USER -s api_token -w SomeAPItoken login.keychain
+# security add-generic-password -a $USER -s awx_token -w SomeAPItoken login.keychain
 	if is-darwin; then
 		export CONTROLLER_TOKEN=$(security find-generic-password -a $USER -s awx_token -w login.keychain)
 	fi
 fi
 
-if (( ${+commands[ara]} )); then
-# Reasonably modern ara module installation, which implies callback plugin availability
-	export ANSIBLE_CALLBACK_PLUGINS="$(python3 -m ara.setup.callback_plugins)"
-fi
+# Similarly, ARA should be handled by direnv now
+# if (( ${+commands[ara]} )); then
+# # Reasonably modern ara module installation, which implies callback plugin availability
+# 	export ANSIBLE_CALLBACK_PLUGINS="$(python3 -m ara.setup.callback_plugins)"
+# fi
