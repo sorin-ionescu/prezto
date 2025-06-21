1df534eaaca16e172762f949543c7402b5627a38
Nguyễn Huỳnh Anh Khoa <113995598+anhkhoakz@users.noreply.github.com>
2025-06-21 17:51:29 +0700

    Using builtin cd
    Using builtin cd to prevent zoxide --cmd=cd conflict cause:
    zoxide: no match found

--- init.zsh
+++ init.zsh
@@ -29,7 +29,7 @@ function zprezto-update {
       printf "running\ncd '%s' and then\n'git pull' " "${ZPREZTODIR}"
       printf "to manually pull and possibly merge in changes\n"
     }
-    cd -q -- "${ZPREZTODIR}" || return 7
+    builtin cd -q -- "${ZPREZTODIR}" || return 7
     local orig_branch="$(git symbolic-ref HEAD 2> /dev/null | cut -d '/' -f 3)"
     if [[ "$orig_branch" == "master" ]]; then
       git fetch || return "$?"
